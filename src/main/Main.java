a3bedcd2457acdf56e182f1e97a8f90c8fa371fd
Dan Deng <danthaman44@gmail.com>
2014-04-12 00:00:24 -0400

    added naive word count

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,5 @@
 package main;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 
@@ -11,47 +12,52 @@ import java.util.*;
  */
 public class Main
 {
-    public static void main (String[] args)
-    {
-        EndSystem myEndSystem = new EndSystem();
-
-        Scanner in = new Scanner(System.in);
-
-        System.out.println("Please enter command host or join: ");
-
-        while(true){
-            String action = in.next();
-            if (action.equals("host")) {
-                String port = in.next();
-                myEndSystem.hostNetwork(port);
-                System.out.printf("Hosting a network at port %d!\n", Integer.parseInt(port));
-                break;
-            }
-            else if (action.startsWith("join")) {
-                myEndSystem.joinHost(in.next(), in.next(), in.next(), in.next());
-                System.out.println("JOIN request sent to network host!");
-                break;
-            }
-            else {
-                System.out.println("Unrecognized action... Error!");
-            }
-        }
-
-        while (true) {
-        // only runs once for now, add the loop later
-        
-            System.out.println("Please enter the next command: mrWordCount/more commands coming");
-            String action2 = in.next();
-
-            if (action2.equals("mrWordCount")){
-                String file = in.next();
-                System.out.println("Carrying out word count on file " + file);
-                
-                myEndSystem.runWordCount(file);
-                break;
-            }
-            
-        }
-        in.close();
+    public static void main (String[] args) throws FileNotFoundException
+    {      
+    NaiveWordCount counter = new NaiveWordCount();
+    counter.countWords("test.txt");
+    counter.printWordCount();
+//        EndSystem myEndSystem = new EndSystem();
+//
+//        Scanner in = new Scanner(System.in);
+//
+//        System.out.println("Please enter command 'host' or 'join': ");
+//
+//        while(true){
+//            String action = in.next();
+//            if (action.equals("host")) {
+//                System.out.println("Please enter a port number: ");
+//                String port = in.next();
+//                myEndSystem.hostNetwork(port);
+//                System.out.printf("Hosting a network at port %d!\n", Integer.parseInt(port));
+//                break;
+//            }
+//            else if (action.startsWith("join")) {
+//                myEndSystem.joinHost(in.next(), in.next(), in.next(), in.next());
+//                System.out.println("JOIN request sent to network host!");
+//                break;
+//            }
+//            else {
+//                System.out.println("Unrecognized action... Error!");
+//            }
+//        }
+//
+//        while (true) {
+//        // only runs once for now, add the loop later
+//        
+//            System.out.println("Please enter the next command: mrWordCount/more commands coming");
+//            String action2 = in.next();
+//
+//            if (action2.equals("mrWordCount")){
+//                String file = in.next();
+//                System.out.println("Please enter a file name: ");
+//                System.out.println("Carrying out word count on file " + file);
+//                
+//                myEndSystem.runWordCount(file);
+//                break;
+//            }
+//            
+//        }
+//        in.close();
     }
 }
---
+++ src/main/NaiveWordCount.java
@@ -0,0 +1,52 @@
+package main;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.io.*;
+
+public class NaiveWordCount {
+    
+    private HashMap<String, Integer> map;
+    private static Scanner s;
+    private static final String root = "src/files/";
+    private static final Pattern PUNCTUATION = Pattern.compile("[\"(){},.;!?<>%]");
+    
+    public NaiveWordCount() {
+        map = new HashMap<>();
+    }
+    
+    public void countWords(String file) throws FileNotFoundException {
+        file = root+file;
+        File fileName = new File(file);
+        try {
+            s = new Scanner(fileName);
+            while (s.hasNext()) {
+                String rawWord = s.next();
+                String word = removePunctiation(rawWord);
+                if (map.containsKey(word)) {
+                    int count = map.get(word);
+                    count++;
+                    map.put(word, count);
+                }
+                else {
+                    map.put(word, 1);
+                }
+            }
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+        }    
+    }
+    
+    private static String removePunctiation(String x) {
+        return PUNCTUATION.matcher(x).replaceAll("");
+    }
+    
+    public void printWordCount() {
+        for (String word: map.keySet()) {
+            int count = map.get(word);
+            System.out.println(word + " : "+ count);
+        }
+    }
+
+}
